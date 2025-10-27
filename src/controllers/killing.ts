68442a98a72bc2b4d3edf10d2a6fb7faa479849e
QwiglyDee <qwiglydee@gmail.com>
2025-10-28 02:51:04 +0700

    killing ctrl

---
+++ src/controllers/killing.ts
@@ -0,0 +1,28 @@
+import { KeyboardEventTypes, type KeyboardInfo } from "@babylonjs/core/Events/keyboardEvents";
+import type { Mesh } from "@babylonjs/core/Meshes/mesh";
+
+import { BabylonController } from "./base";
+
+export class KillingController extends BabylonController {
+    radius: number = 3;
+
+    #obs: any;
+
+    init() {
+        this.#obs = this.scene.onKeyboardObservable.add((info: KeyboardInfo) => {
+            if (info.type == KeyboardEventTypes.KEYDOWN && info.event.key == 'x' && this.picked) this.#kill(this.picked);
+        });
+    }
+
+    dispose() {
+        if (this.#obs) this.#obs.remove();
+    }
+    
+    updating() {}
+    update() {}
+
+    #kill(mesh: Mesh) {
+        mesh.dispose();
+        this.scene.onModelUpdatedObservable.notifyObservers([mesh]);
+    }
+}
