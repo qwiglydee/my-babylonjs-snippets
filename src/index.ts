7c145913561086439a3635ef946c2caa7f34791a
QwiglyDee <qwiglydee@gmail.com>
2025-10-27 20:34:48 +0700

    ghost component

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import "./our-app";
 import "./our-status";
 
 import "./my-babylon";
-import "./my-layer";
-import "./my-stuff";
-import "./my-axes";
-import "./my-ground-grid";
-import "./my-ground-flat";
-import "./my-something";
 import "./my-camera-arc";
 import "./my-environ";
+import "./my-ghost";
+import "./my-ground-flat";
+import "./my-ground-grid";
+import "./my-layer";
+import "./my-something";
+import "./my-stuff";
+import "./my-axes";
+
 import "./my-highlighter";
---
+++ src/my-ghost.ts
@@ -0,0 +1,48 @@
+import { consume } from "@lit/context";
+import { ReactiveElement, type PropertyValues } from "lit";
+import { customElement, state } from "lit/decorators.js";
+
+import type { PickingInfo } from "@babylonjs/core/Collisions/pickingInfo";
+import { BackgroundMaterial } from "@babylonjs/core/Materials/Background/backgroundMaterial";
+import { CreateBox } from "@babylonjs/core/Meshes/Builders/boxBuilder";
+import { Mesh } from "@babylonjs/core/Meshes/mesh";
+import type { Scene } from "@babylonjs/core/scene";
+import type { Nullable } from "@babylonjs/core/types";
+
+import { GhostBehavior } from "./behaviors/ghost";
+import { pickCtx, sceneCtx } from "./context";
+
+@customElement("my-ghost")
+export class MyGhostElem extends ReactiveElement {
+    @consume({ context: sceneCtx, subscribe: false })
+    scene!: Scene;
+
+    @consume({ context: pickCtx, subscribe: true })
+    @state()
+    pick: Nullable<PickingInfo> = null;
+
+    override connectedCallback(): void {
+        super.connectedCallback();
+        this.#init();
+    }
+
+    _mesh!: Mesh;
+    _bhv!: GhostBehavior;
+    
+    #init() {
+        this._mesh = CreateBox("(ghost)", {}, this.scene);
+        this._mesh.isPickable = false;
+        this._mesh.material = new BackgroundMaterial("(ghost)", this.scene);
+        this._mesh.material.alpha = 0.25;
+        
+        this._bhv = new GhostBehavior();
+        this._bhv.attach(this._mesh);
+    }
+
+    override update(changes: PropertyValues) {
+        if (changes.has('pick')) {
+            this._bhv.targetMesh = this.pick ? this.pick.pickedMesh : null;
+        }
+        super.update(changes);
+    }
+}
